e2438d2c45cd4991943667b1bd0c1a596c367d30
lvenneri <lorenzo.venneri@gmail.com>
2025-12-18 23:32:51 -0500

    updated file structure for better imports

---
+++ src/casegenmc/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.14"
+
+from .core import *
+from .NEORL_wrap import (
+    create_NEORL_funwrap,
+    NEORL_getbounds,
+)
+from .scipy_wrap import (
+    create_scipy_funwrap,
+    get_scipy_bounds,
+)
